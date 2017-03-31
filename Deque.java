5c82c7c5365107fdad584a18bda3e4db311923a8
Yuqiwu <ywu@stuy.edu>
2017-03-31 08:56:02 -0400

    added interface

---
+++ Deque.java
@@ -0,0 +1,30 @@
+/*
+  Team Name: QQ
+  Team Members: Yu Qi Wu, Ri Hui Zheng,
+  APCS2 pd3
+  LAB02 All Hands on Deque!
+ */
+
+public interface Deque<E>{
+    public boolean add(E e);
+    
+    public void addFirst(E e);
+    
+    public void addLast(E e);
+
+    public E peek();
+    
+    public E peekFirst();
+    
+    public E peekLast();
+    
+    public E pop();
+    
+    public E remove();
+
+    public e removeFirst();
+
+    public e removeLast();
+
+    public int size();
+}
