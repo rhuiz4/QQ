dd65cbabbfb62216f7917e8de99b9490065a6615
Rihui Zheng <rzheng4@stuy.edu>
2017-03-31 07:15:04 -0400

    instance vars

--- QQ.java
+++ QQ.java
@@ -3,8 +3,13 @@
 
 import java.util.ArrayList;
 public class QQ implements Deque{
+    private ArrayList _front, _end;
+    private int _size;
 
-    public void QQ
+    public void QQ {
+	_front = _end = null;
+	_size = 0;
+    }
     public boolean add(E e){
 	
     }
